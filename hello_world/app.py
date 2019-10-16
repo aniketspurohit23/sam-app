36561c1474ee81e8cefe4e436c58123b61055fda
aniketspurohit23 <aniket.purohit@aptusdatalabs.com>
2019-10-16 17:42:27 +0530

    Update app.py

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "Hello Akhilesh & smriti Rocks",
+            "message": "Hello Akhilesh",
             # "location": ip.text.replace("\n", "")
         }),
     }
